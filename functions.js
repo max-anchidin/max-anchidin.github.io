f1fecc98b1998609691836d708765e360ea1d610
Max Anchidin <anchidinm@gmail.com>
2021-06-24 21:20:47 +0300

    clean up unused code

--- functions.js
+++ functions.js
@@ -1,21 +1,3 @@
-console.debug('inside functions.js');
-
-function getInfo(
-    name) {
-    var msg = "Salut " + name;
-    console.warn('inside get info', msg);
-    return msg;
-}
-
-console.info(getInfo("OK " + "Whatever"));
-console.warn(getInfo("OK " + "Whatever"));
-
-var myName = "OK " + "Whatever";
-var welcome = getInfo(myName);
-console.info(welcome);
-
-
-
 function showHome() {
     document.getElementById('home').style.display = 'block';
     document.getElementById('skills').style.display = 'none';
