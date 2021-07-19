95dd6fe655f09427e3463f7b11dbef8d3c97d514
Max Anchidin <anchidinm@gmail.com>
2021-07-19 19:40:45 +0300

    use const and let instead of var

--- functions.js
+++ functions.js
@@ -18,23 +18,23 @@ function hide(id) {
 }
 
 function show(id) {
-    var page = document.getElementById(id);
+    const page = document.getElementById(id);
     if (page) {
         page.style.display = 'block';
         highlight(page);
     } else {
         console.warn("pagina cu id-ul %o nu exista", id);
     }
-    var oldLink = document.querySelector("a[data-page].active");
+    const oldLink = document.querySelector("a[data-page].active");
     if (oldLink) {
         oldLink.classList.remove("active");
     }
-    var link = document.querySelector(`a[data-page=${id}]`);
+    const link = document.querySelector(`a[data-page=${id}]`);
     link.classList.add("active");
 }
 
 function hideAllPages() {
-    var pages = Array.from(document.getElementsByClassName('mainbox'));
+    const pages = Array.from(document.getElementsByClassName('mainbox'));
 
     pages.forEach(function (page) {
         hide(page.id);
@@ -50,18 +50,18 @@ show('skills');
 
 document.querySelector('#top-menu-bar').addEventListener('click', function (e) {
     if (e.target.matches('a')) {
-        var id = e.target.getAttribute('data-page');
+        const id = e.target.getAttribute('data-page');
         showPage(id);
         highlight(e.target);
     }
 })
 
-var skills = [];
+window.skills = [];
 
 function showSkills(skills) {
-    var skillsHtml = skills.map(function (skill) {
-        var favourite = skill.favourite ? 'class="favourite"' : '';
-        var endorsements = skill.endorsements > 5 ? `<span>${skill.endorsements}</span>` : '';
+    const skillsHtml = skills.map(function (skill) {
+        const favourite = skill.favourite ? 'class="favourite"' : '';
+        const endorsements = skill.endorsements > 5 ? `<span>${skill.endorsements}</span>` : '';
         return `<li ${favourite}>${skill.name} ${endorsements}</li>`;
     }).join('');
 
@@ -69,8 +69,8 @@ function showSkills(skills) {
 }
 
 function sortSkillsByName(a, b) {
-    var aName = a.name.toUpperCase();
-    var bName = b.name.toUpperCase();
+    const aName = a.name.toUpperCase();
+    const bName = b.name.toUpperCase();
     if (aName < bName) {
         return -1;
     }
